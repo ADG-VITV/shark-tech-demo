ff6572f47001853a9e4cb97ab822fc57dec8b8fd
SBeeeee <shatadru017@gmail.com>
2025-02-05 12:46:37 +0530

    footer added and minor changes done

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="ml-16 pl-16"> 
+      <img src="Group 6.png" className="w-72"></img>
+    </div>
+  )
+}
